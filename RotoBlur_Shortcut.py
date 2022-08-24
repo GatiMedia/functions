282d86c18915a607e44e86ca9428a623873917e6
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2022-08-24 17:06:10 +0100

    Create RotoBlur_Shortcut.py

---
+++ RotoBlur_Shortcut.py
@@ -0,0 +1,13 @@
+def RotoBlur_Shortcut():
+    y_offset = 60
+    r = nuke.createNode('Roto', 'output alpha')
+    y = int(r.ypos() + y_offset)
+    b = nuke.nodes.Blur()
+    b['size'].setValue(2)
+    b['label'].setValue('Size: [value size]')
+    b['xpos'].setValue(r.xpos())
+    b['ypos'].setValue(y)
+    b.setInput(0,r)
+    b.hideControlPanel()
+    
+nuke.menu('Nodes').addMenu('Draw').addCommand('Create Roto and Blur node', 'RotoBlur_Shortcut()', shortcut='o', icon='Roto.png')
