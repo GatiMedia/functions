333ed0b21bd039568001ceb875e406ff9defdbdb
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2022-08-24 17:04:43 +0100

    Create disconnectViewers.py

---
+++ disconnectViewers.py
@@ -0,0 +1,11 @@
+def disconnectViewers():
+    nuke.selectAll()
+    nuke.invertSelection()
+
+    for n in nuke.allNodes():
+        if n.Class() == "Viewer":
+            n['selected'].setValue(True)
+
+    nuke.extractSelected()
+
+nuke.addOnScriptLoad(disconnectViewers)
