ae1c42c8f6a49bff2f0d5da13eec8de047da7460
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2022-10-20 17:19:14 +0100

    Create getTileColor.py

---
+++ getTileColor.py
@@ -0,0 +1,42 @@
+def getTileColor(nodes):
+    nodes = nuke.selectedNodes()
+    if nodes:
+        nodeNames = []
+        nodeColors = []
+        nodeInfo = []
+        for n in nodes:
+            nClass = n.Class()
+            tileColorNew = n['tile_color'].value()
+            if tileColorNew == 0:
+                tileColorNew = None
+                ###cache
+                class02 = [i.capitalize() for i in nuke.toNode('preferences')['NodeColourClassCache'].value().split()]
+                for c in class02:
+                    if c in nClass:
+                        tileColorNew = nuke.toNode('preferences')['NodeColourCacheColor'].value()
+                ###deep
+                class02 = [i.capitalize() for i in nuke.toNode('preferences')['NodeColourClassDeep'].value().split()]
+                for c in class02:
+                    if c in nClass:
+                        tileColorNew = nuke.toNode('preferences')['NodeColourDeepColor'].value()
+                ###01-14
+                for num in range(14):
+                    className = 'NodeColourClass'+str(num+1).zfill(2)
+                    classColor = 'NodeColour'+str(num+1).zfill(2)+'Color'
+                    for c in [i.capitalize() for i in nuke.toNode('preferences')[className].value().split()]:
+                        if c in nClass:
+                            tileColorNew = nuke.toNode('preferences')[classColor].value()
+                    for c in [i for i in nuke.toNode('preferences')[className].value().split()]:
+                        if c in nClass:
+                            tileColorNew = nuke.toNode('preferences')[classColor].value()
+                if tileColorNew == None:
+                    tileColorNew = nuke.toNode('preferences')['NodeColour14Color'].value()
+            nodeNames.append(n.name())
+            nodeColors.append(tileColorNew)
+        for name, color in zip(nodeNames, nodeColors):
+            nodeInfo.append(str(name)+" : "+str(color))
+    else:
+        nuke.message('<font color=orange><b>\n\nSelect some nodes first!\n\n')
+    return(nodeInfo)
+
+print(getTileColor(nuke.selectedNodes()))
