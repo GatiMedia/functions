0f0379a86764a8b4e295f3c91793cff69b70b939
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2023-01-03 12:59:48 +0000

    Update find_animated_nodes.py

--- find_animated_nodes.py
+++ find_animated_nodes.py
@@ -26,15 +26,21 @@ class animNodesWindow(nukescripts.PythonPanel):
         self.addKnob(self.spaceKnob)
 
         self.infoKnob = nuke.Text_Knob('info', '')
-        self.infoKnob.setValue('<p style="font-size:20px">&#128269; Select an Animated Node</p>')
+        self.infoKnob.setValue('<p style="font-size:15px">&#128269; Select an Animated Node to Zoom on it!</p>')
         self.addKnob(self.infoKnob)
+
+        self.spaceKnob2 = nuke.Text_Knob('space3', '')
+        self.addKnob(self.spaceKnob2)
 
 ### Open the popup window
 def openAnimNodesWindow():
-    anim_nodes_window = animNodesWindow()
-    if anim_nodes_window.showModalDialog():
-        selected_node = anim_nodes_window.nodeNamesKnob.value()
-        for node in nuke.allNodes():
-            node.setSelected(False)
-        nuke.toNode(selected_node).setSelected(True)
-        nuke.zoomToFitSelected()
+    if getAnimNodes():
+        anim_nodes_window = animNodesWindow()
+        if anim_nodes_window.showModalDialog():
+            selected_node = anim_nodes_window.nodeNamesKnob.value()
+            for node in nuke.allNodes():
+                node.setSelected(False)
+            nuke.toNode(selected_node).setSelected(True)
+            nuke.zoomToFitSelected()
+    else:
+        nuke.message('''<center><p style="font-size:30px">&#128531; <p style="font-size:15px"> Didn't find any animated nodes!</p>''')
